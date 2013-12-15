65d5d872a92ae70b9d0a1096a7cc6390a6108e34
Juline <juline.lamusga@gmail.com>
2013-12-15 14:14:11 -0600

    app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capstone::Application.config.secret_token = 'a240e385cdfc00b5632bcafa6d7ca1d6e6f002ff6b69a6fe35ec09532b29728a21f8da06b205eef26326416911023cb45c7080ab4da4c5d25780e98cd89a3cdd'
